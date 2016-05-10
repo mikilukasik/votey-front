f6df762b991924b1e6e0ab48ca0c32fa688526e1
miki-rga <miklos.lukasik@rentguard.co.uk>
2016-05-10 17:01:27 +0100

    trdirective ok now

--- www/js/directives/clickForOptionsDirective.js
+++ www/js/directives/clickForOptionsDirective.js
@@ -1,51 +1,47 @@
 //source  
 //
 //https://forum.ionicframework.com/t/ion-option-button-in-list-toggle-by-icon-click-instead-of-swipe/9703/4
-
 app.directive('clickForOptions', ['$ionicGesture', function($ionicGesture) {
-    return {
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-            var thisFunc = function(){
-                if (!scope.$eval(attrs.alwaysShowButtons)) thisFunc2()
-            }
-            var thisFunc2 = function(e){
-                // Grab the content
-                content = element[0].querySelector('.item-content');
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
 
-                // Grab the buttons and their width
-                buttons = element[0].querySelector('.item-options');
+      var buttons = element[0].querySelector('.item-options');
+      var content = element[0].querySelector('.item-content');
 
+      var thisFunc = function() {
+        if (!scope.$eval(attrs.alwaysShowButtons)) thisFunc2()
+      };
 
-
-                if (!buttons) {
-                    return;
-                }
-                var buttonsWidth = buttons.offsetWidth;
-                var contentWidth = content.offsetWidth;
-
-                ionic.requestAnimationFrame(function() {
-                    content.style[ionic.CSS.TRANSITION] = 'all ease-out .25s';
-
-                    if (!buttons.classList.contains('invisible')) {
-
-                        content.style.width = contentWidth + buttonsWidth +'px';
-                        
-                        // content.style[ionic.CSS.TRANSFORM] = '';
-                        // setTimeout(function() {
-                            buttons.classList.add('invisible');
-                        // }, 250);                
-                    } else {
-                        buttons.classList.remove('invisible');
-                        
-                        //content.style[ionic.CSS.TRANSFORM] = 'translate3d(-' + buttonsWidth + 'px, 0, 0)';
-                        content.style.width = contentWidth - buttonsWidth +'px';
-                    }
-                });     
-
-            }
-            $ionicGesture.on('tap', thisFunc, element);
-          
+      var thisFunc2 = function(e) {
+        if (!buttons) {
+          return;
         }
-    };
+
+        var buttonsWidth = buttons.offsetWidth;
+        var contentWidth = content.offsetWidth;
+
+        ionic.requestAnimationFrame(function() {
+          content.style[ionic.CSS.TRANSITION] = 'all ease-out .25s';
+
+          if (!buttons.classList.contains('invisible')) {
+            content.style.width = '100%';//contentWidth + buttonsWidth + 'px';
+            buttons.classList.add('invisible');
+          } else {
+            content.style.width = contentWidth - buttonsWidth + 'px';
+            buttons.classList.remove('invisible');
+          }
+        });
+
+      };
+
+      $ionicGesture.on('tap', thisFunc, element);
+   
+      buttons.style[ionic.CSS.TRANSITION] = 'all ease-out .25s';
+
+      buttons.classList.remove('invisible');
+      thisFunc2();
+
+    }
+  };
 }])
